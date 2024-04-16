98963d8047817dd33caad1e4231becba1da289d3
MohanRajA9 <akmohanraj555@gmail.com>
2024-04-16 15:05:01 +0530

    finished basics of formik and yup.js

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -3,73 +3,88 @@ import { useNavigate } from 'react-router-dom';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { API } from '../global';
+import { useFormik } from 'formik';
+import *as yup from 'yup'
+
+const produtvalidationSchema = yup.object({
+  name: yup.string()
+    .required("why not fill the name?"),
+  poster: yup.string()
+    .min(20, "Need a longer poster")
+    .required("why not fill the poster?"),
+  price: yup.number()
+    .required("why not fill the price?"),
+  summary: yup.string()
+    .min(20, "Need a longer summary")
+    .required("why not fill the summary?"),
+  ratings: yup.number()
+    .min(1, "ratings should be above one")
+    .max(5, "ratings should be below five")
+    .required("why not fill the rating?")
+})
 
 export function AddProduct() {
-
-  const [name, setName] = useState("");
-  const [poster, setPoster] = useState("");
-  const [price, setPrice] = useState("");
-  const [summary, setSummary] = useState("");
-  const [ratings, setRatings] = useState("");
   const navigate = useNavigate()
 
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      poster: "",
+      price: "",
+      summary: "",
+      ratings: ""
+    },
+    validationSchema: produtvalidationSchema,
+    onSubmit: (newProduct) => {
+      console.log("onSubmit", newProduct)
+      CreateProduct(newProduct)
+    }
+  })
+
+  function CreateProduct(newProduct) {
+    console.log("createProduct", newProduct)
+    fetch(`${API}/products`, {
+      method: "POST",
+      body: JSON.stringify(newProduct),
+      headers: {
+        "content-Type": "application/json"
+      }
+    })
+      .then((res) => res.json())
+      .then(() => navigate("/products"))
+  }
 
   return (
-    <div className='add-product-form' >
-      <h1>Add Product</h1>
 
-      <TextField id="outlined-basic" label="Name" variant="outlined" value={name} onChange={(event) => setName(event.target.value)} />
+    <form onSubmit={formik.handleSubmit} >
 
-      <TextField id="outlined-basic" label="Poster" variant="outlined" value={poster} onChange={(event) => setPoster(event.target.value)} />
+      <div className='add-product-form' >
+        <h1>Add Product</h1>
 
-      <TextField id="outlined-basic" label="Price" variant="outlined" value={price} onChange={(event) => setPrice(event.target.value)} />
-
-      <TextField id="outlined-basic" label="Summary" variant="outlined" value={summary} onChange={(event) => setSummary(event.target.value)} />
-
-      <TextField id="outlined-basic" label="Ratings" variant="outlined" value={ratings} onChange={(event) => setRatings(event.target.value)} />
-
-      <Button variant="contained"
-        onClick={() => {
-          const newProduct = {
-            name,
-            poster,
-            price,
-            summary,
-            ratings
-          }
-          fetch(`${API}/products`, {
-            method: "POST",
-            body: JSON.stringify(newProduct),
-            headers: {
-              "Content-Type": "application/json"
-            }
-          })
-            .then((res) => res.json())
-            .then(() => navigate("/products"))
-          // setProductList([...productList, newProduct])
-          // console.log(newProduct)
-
-        }}>Add Product</Button>
+        <TextField id="name" label="Name" variant="outlined" name="name" value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+        <br />
+        {formik.touched.name && formik.errors.name ? formik.errors.name : ""}
 
 
+        <TextField id="poster" label="Poster" variant="outlined" name="poster" value={formik.values.poster} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+        <br />
+        {formik.touched.poster && formik.errors.poster ? formik.errors.poster : ""}
 
-      {/* <button 
-     onClick={ () => { 
-      const newProduct = {
-        name,
-        poster,
-        price,
-        summary,
-        ratings
-     }
-      
-      setProductList([...productList, newProduct])
-      console.log(newProduct)
-      navigate("/products")
-      
-     } }
-     >Add Product</button> */}
+        <TextField id="price" label="Price" variant="outlined" name="price" value={formik.values.price} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+        <br />
+        {formik.touched.price && formik.errors.price ? formik.errors.price : ""}
 
-    </div>
+
+        <TextField id="summary" label="Summary" variant="outlined" name="summary" value={formik.values.summary} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+        <br />
+        {formik.touched.summary && formik.errors.summary ? formik.errors.summary : ""}
+
+        <TextField id="ratings" label="Ratings" variant="outlined" name="ratings" value={formik.values.ratings} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+        <br />
+        {formik.touched.ratings && formik.errors.ratings ? formik.errors.ratings : ""}
+
+        <Button variant="contained" type="submit" onClick={CreateProduct} >Add Product</Button>
+      </div>
+    </form>
   );
 }
